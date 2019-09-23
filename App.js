72b7853d38ea11e9a0757b7bc81b8ec92d9ae4e1
yenny <aldald73@naver.com>
2019-09-23 23:51:38 +0900

    login & chart

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, Alert } from 'react-native';
 import Main from './Main.js';
 
 var id, password;
@@ -9,34 +9,42 @@ export default class extends React.Component{
   };
 
   render(){
-  return (
-    this.state.login ? <Main></Main> :
-    <View style={styles.container}>
-      <Image style={styles.logo} source={require('./assets/VaRoon.png')}></Image>
-      <TextInput style={styles.input}
-      underlineColorAndroid="transparent"
-      placeholder="ID"
-      placeholderTextColor="#A8BAFA"
-      onChangeText={text => id = text}
-      ></TextInput>
-      <TextInput style={styles.input} 
-      secureTextEntry={true}
-      underlineColorAndroid="transparent"
-      placeholder="PASSWORD"
-      placeholderTextColor="#A8BAFA"
-      onChangeText={text => password = text}
-      ></TextInput>
-      <TouchableOpacity style={styles.loginButton} onPressOut={this._loginAuth}>
-        <Text style={{color: '#fff'}}>LOGIN</Text>
-      </TouchableOpacity>
-    </View>
+    return (
+      this.state.login ? <Main></Main> :
+        <View style={styles.container}>
+          <View style={styles.halfContainer1}>
+            <Image style={styles.logo} source={require('./assets/VaRoon.png')}></Image>
+          </View>
+          <View style={styles.halfContainer2}>
+            <TextInput style={styles.input}
+              underlineColorAndroid="transparent"
+              placeholder="ID"
+              placeholderTextColor="#A8BAFA"
+              onChangeText={text => id = text}
+            ></TextInput>
+            <TextInput style={styles.input}
+              secureTextEntry={true}
+              underlineColorAndroid="transparent"
+              placeholder="PASSWORD"
+              placeholderTextColor="#A8BAFA"
+              onChangeText={text => password = text}
+            ></TextInput>
+            <TouchableOpacity style={styles.loginButton} onPressOut={this._loginAuth}>
+              <Text style={{ color: '#fff' }}>LOGIN</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
     );
   }
   _loginAuth = () => {
-    console.log(id, password);
-    //로그인 성공시
-    this.setState({login: true});
-
+    if(id == undefined|| id == "" || password == undefined || password == "")
+    {
+      Alert.alert("Enter your ID and password")
+    }
+    else{
+      //로그인 성공시
+      this.setState({login: true});
+    }
   }
 }
 
@@ -46,16 +54,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
   },
+  halfContainer1: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  halfContainer2: {
+    flex: 2,
+    alignItems: 'center',
+  },
   logo: {
-    marginTop: 150,
-    marginBottom:  50,
-    width: 200,
-    height: 200,
+    width: 150,
+    height: 150,
     resizeMode: 'contain'
 
   },
   input:{
-    marginTop: 30,
+    marginTop: 10,
     height: 50,
     width: 300,
     borderBottomWidth: 1,
@@ -64,7 +79,7 @@ const styles = StyleSheet.create({
     color: '#5276F6'
   },
   loginButton:{
-    marginTop: 60,
+    marginTop: 50,
     width: 130,
     height: 50,
     backgroundColor: '#5276F6',
--- Main.js
+++ Main.js
@@ -1,13 +1,54 @@
 import React from 'react';
-import { StyleSheet, Text} from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Dimensions, Platform,ScrollView} from 'react-native';
+
+const {width, height} = Dimensions.get("window");
 
 export default class Main extends React.Component{
     render(){
         return(
-            <Text>
-                login successful!
-            </Text>
-
+            <View style={styles.container}>
+                <StatusBar barStyle="light-content"></StatusBar>
+                <Text style={styles.title}>Chart</Text>
+                <ScrollView style={styles.chart}>
+                </ScrollView>
+            </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: '#5276F6',
+        alignItems: 'center'
+    },
+    title:{
+        color: 'white',
+        fontSize: 40,
+        marginTop: 50,
+        fontWeight: "200",
+        marginBottom: 20
+    },
+    chart:{
+        backgroundColor: "white",
+        flex: 1,
+        width: width - 25,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        ...Platform.select({
+            ios: {
+                shadowColor: "rgb(50,50,50)",
+                shadowOpacity: 0.5,
+                shadowRadius: 10,
+                shadowOffset:{
+                    height: -2,
+                    width: 0
+                }
+
+            },
+            android: {
+                elevation: 10
+            }
+        })
+    }
+})
